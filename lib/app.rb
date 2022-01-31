cc3076b56eb8a32b7056d6f54220487cb8d01b8e
Sonali Kar <sonali.kar87@gmail.com>
2022-01-31 23:08:53 +0000

    first commit

---
+++ lib/app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  'This is a secret page..... edited..edited again'
+end
+
+get '/anotherpage' do
+  "Extra page"
+end
+
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
